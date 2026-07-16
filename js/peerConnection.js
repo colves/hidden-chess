50ccda700e1efb92be41d051d916534b03391cd7
colves <126682212+colves@users.noreply.github.com>
2026-07-16 23:04:27 +0300

    Add files via upload

---
+++ js/peerConnection.js
@@ -0,0 +1,82 @@
+class PeerConnection {
+    constructor(onMessageReceived, onConnectionStatusChanged) {
+        this.peer = null;
+        this.connection = null;
+        this.isHost = false;
+        this.onMessageReceived = onMessageReceived;
+        this.onConnectionStatusChanged = onConnectionStatusChanged; // Connecting, Connected, Disconnected
+    }
+
+    initHost() {
+        return new Promise((resolve, reject) => {
+            this.isHost = true;
+            // Generate a random 6-character alphanumeric code
+            const code = Math.random().toString(36).substring(2, 8).toUpperCase();
+            
+            this.peer = new Peer('hc-' + code);
+
+            this.peer.on('open', (id) => {
+                const displayCode = id.replace('hc-', '');
+                resolve(displayCode);
+            });
+
+            this.peer.on('connection', (conn) => {
+                this.connection = conn;
+                this.setupConnection();
+            });
+
+            this.peer.on('error', (err) => {
+                console.error("PeerJS error:", err);
+                reject(err);
+            });
+        });
+    }
+
+    joinGame(code) {
+        return new Promise((resolve, reject) => {
+            this.isHost = false;
+            this.peer = new Peer();
+
+            this.peer.on('open', () => {
+                this.connection = this.peer.connect('hc-' + code);
+                
+                this.connection.on('open', () => {
+                    this.setupConnection();
+                    resolve(true);
+                });
+
+                this.connection.on('error', (err) => {
+                    console.error("Connection error:", err);
+                    reject(err);
+                });
+            });
+
+            this.peer.on('error', (err) => {
+                console.error("PeerJS error:", err);
+                reject(err);
+            });
+        });
+    }
+
+    setupConnection() {
+        this.onConnectionStatusChanged('Connected');
+
+        this.connection.on('data', (data) => {
+            if (this.onMessageReceived) {
+                this.onMessageReceived(data);
+            }
+        });
+
+        this.connection.on('close', () => {
+            this.onConnectionStatusChanged('Disconnected');
+        });
+    }
+
+    sendMessage(type, payload) {
+        if (this.connection && this.connection.open) {
+            this.connection.send({ type, payload });
+        } else {
+            console.warn("Cannot send message: Connection not open");
+        }
+    }
+}
